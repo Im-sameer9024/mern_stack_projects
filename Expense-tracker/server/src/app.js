2dc04bc4d6cf896938c142934cd1669823dbd701
Mohammed Sameer <rskkhan89@gmail.com>
2026-04-07 17:18:35 +0530

    udpate

--- Expense-tracker/server/src/app.js
+++ Expense-tracker/server/src/app.js
@@ -12,7 +12,9 @@ import transactionRoute from './modules/transaction/transaction.route.js';
 
 const app = express();
 
-const allowedOrigins = ['http://localhost:3000', '*'];
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',')
+
+console.log(allowedOrigins)
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
@@ -20,14 +22,13 @@ app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(
   cors({
-    // origin: (origin, callback) => {
-    //   if (!origin || allowedOrigins.includes(origin)) {
-    //     callback(null, true);
-    //   } else {
-    //     callback(new Error("Not allowed by CORS"));
-    //   }
-    // },
-    origin: true, // for developmemnt
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
