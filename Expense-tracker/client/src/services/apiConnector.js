08ccb924f3935fafebbb8e567e9f93dec66c1ef9
Mohammed Sameer <rskkhan89@gmail.com>
2026-04-13 23:47:57 +0530

    vercel.json file update

--- Expense-tracker/client/src/services/apiConnector.js
+++ Expense-tracker/client/src/services/apiConnector.js
@@ -35,7 +35,7 @@ axiosInstance.interceptors.response.use(
   },
   async (error) => {
     const originalRequest = error?.config;
-    const { setToken } = useAuthStore.getState();
+    const { setToken, clearToken } = useAuthStore.getState();
 
     // ❌ Network / unknown error
     if (!error?.response || !originalRequest) {
@@ -53,7 +53,7 @@ axiosInstance.interceptors.response.use(
       //---------------------if already refreshing token -----------------
 
       if (isRefreshing) {
-        return new Promise(() => {});
+        return  new Promise(() => {});
       }
 
       //--------------- start Refresh--------------------------
@@ -84,6 +84,10 @@ axiosInstance.interceptors.response.use(
           isLoggingOut = true;
 
           await AuthApiOperations.LogoutUser();
+          clearToken();
+          localStorage.removeItem('temp');
+          // redirect user
+          window.location.replace("/login")
         }
         //-------- logout flow ---------------
 
@@ -101,7 +105,7 @@ export const apiConnector = ({ method, url, bodyData, headers, params, responseT
   return axiosInstance({
     method: method,
     url: url,
-    data: bodyData,
+    data: bodyData ,
     headers: headers ? headers : {},
     params: params ? params : {},
     responseType: responseType ? responseType : null,
